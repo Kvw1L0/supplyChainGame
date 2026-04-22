cb326eefc661d652ecd45f0bd4d721695bf48b26
Camilo Martinez <134472412+Kvw1L0@users.noreply.github.com>
2026-04-21 23:05:59 -0400

    Update firebase.js

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "supplychain-2e10b.firebaseapp.com",
-  databaseURL: "https://supplychain-2e10b-default-rtdb.firebaseio.com",
-  projectId: "supplychain-2e10b",
-  storageBucket: "supplychain-2e10b.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
-};
+    apiKey: "AIzaSyCY9_D3Dyh2oRZ4A1EzBPck3RtsDRNkbnY",
+    authDomain: "supplychain-2e10b.firebaseapp.com",
+    databaseURL: "https://supplychain-2e10b-default-rtdb.firebaseio.com",
+    projectId: "supplychain-2e10b",
+    storageBucket: "supplychain-2e10b.firebasestorage.app",
+    messagingSenderId: "877411976306",
+    appId: "1:877411976306:web:862f033082386c3e24eeeb"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const dbFirestore = getFirestore(app);
